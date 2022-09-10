16b848393d331aa44fbd0b7f4a76f078b8fedaf4
Jalen Lamb <2018118881@ub.edu.bz>
2022-09-09 21:33:07 -0600

    Fixed caution in healthcheck

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -5,6 +5,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // createSchoolHandler for the "POST /v1/schools" endpoint
@@ -13,9 +16,14 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 }
 
 // showSchoolHandler for the "GET /v1/schools/:id" endpoint
-/*func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
 	// Use the "ParamsFormContext()" function to get the request context as a slice
 	params := httprouter.ParamsFromContext(r.Context())
-
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Display the school id
+	fmt.Fprintf(w, "show the details for school %d\n", id)
 }
-*/
